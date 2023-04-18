9be14fef0795ab1a3e515a740e4feba380279439
Casian-Gabriel Șuli <ksi0711@yahoo.com>
2023-04-18 18:14:21 +0300

    Initial commit

---
+++ src/main/java/com/example/tparkbe/TParkBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tparkbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TParkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TParkBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tparkbe/TParkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tparkbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TParkBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
