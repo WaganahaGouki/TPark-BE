9cff24ed3366bc494b3c18cc76a3d4ac9c278c76
Casian-Gabriel Șuli <ksi0711@yahoo.com>
2023-06-14 16:29:58 +0300

    Changed login parameters to path variables instead of request body

--- src/main/java/com/example/tparkbe/controller/LoginController.java
+++ src/main/java/com/example/tparkbe/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.example.tparkbe.controller;
 
-import com.example.tparkbe.model.Login;
 import com.example.tparkbe.model.User;
 import com.example.tparkbe.service.LoginService;
 import com.example.tparkbe.service.UserService;
@@ -8,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +18,10 @@ public class LoginController {
     private final UserService userService;
     private final LoginService loginService;
 
-    @GetMapping()
-    public ResponseEntity<User> login(@RequestBody Login login) {
-        User user = userService.findByEmail(login.getEmail());
-        loginService.loginUser(user.getEmail(), login.getPassword());
+    @GetMapping("/{email}/{password}")
+    public ResponseEntity<User> login(@PathVariable("email") String email, @PathVariable("password") String password) {
+        User user = userService.findByEmail(email);
+        loginService.loginUser(user.getEmail(), password);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
